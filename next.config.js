d0c6fea8a2ece5c8ffaa0a1fd4ebc175dbd3b4b1
qryptic <32945281+Kqryptic@users.noreply.github.com>
2026-04-23 09:40:45 -0700

    update next config for github pages subpath compatibility

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/hyperdrive-v2-repository',
+  assetPrefix: '/hyperdrive-v2-repository',
   images: {
     unoptimized: true,
   },
