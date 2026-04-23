8115cc37aa5c4c5bc7150dd7a8df16101316450f
qryptic <32945281+Kqryptic@users.noreply.github.com>
2026-04-23 09:15:33 -0700

    move v2 script to legacy folder

---
+++ legacy/v2.sh
@@ -0,0 +1,7 @@
+echo "# hyperdrive-v2-repository" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Kqryptic/hyperdrive-v2-repository.git
+git push -u origin main
